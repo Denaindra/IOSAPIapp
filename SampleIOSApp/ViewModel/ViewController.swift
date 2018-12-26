0fa2e48ff3ea2b800c779f02d73762c6ceded8ca
mgdperera <mgdperera@gmail.com>
2018-12-26 22:10:58 +0530

    complete FB login function

--- SampleIOSApp/ViewModel/ViewController.swift
+++ SampleIOSApp/ViewModel/ViewController.swift
@@ -1,20 +1,70 @@
-//
-//  ViewController.swift
-//  SampleIOSApp
-//
-//  Created by gayan perera on 12/26/18.
-//  Copyright © 2018 gayan perera. All rights reserved.
-//
 
 import UIKit
+import FacebookLogin
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    //UI outlets
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var Email: UILabel!
+    
+    
+    //properties
+    private let FBButton = FBSDKLoginButton()
+    
+    
+    
+    @IBAction func LoginButtonClick(_ sender: UIButton) {
+        FBButton.sendActions(for: .touchUpInside)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        intialSeteupFBLoginButton()
     }
+    
+    func intialSeteupFBLoginButton() {
+        FBButton.readPermissions = ["public_profile","email"]
+        FBButton.delegate = self
+        if FBSDKAccessToken.current() != nil {
+            ChnageButtonText(btntitle:"Logout")
+        }
+        else {
+            ChnageButtonText(btntitle:"Logging")
+        }
+    }
+    
+    func ChnageButtonText(btntitle:String) {
+        loginButton.setTitle(btntitle, for: .normal)
 
-
+    }
 }
 
+extension ViewController : FBSDKLoginButtonDelegate {
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        if error != nil {
+            print ("Here there is erro \(error)")
+        }
+        else if result.isCancelled{
+            print ("user can cle login ")
+        }
+        else {
+            FBSDKGraphRequest(graphPath:"me", parameters: ["fields":"email,name"]).start(completionHandler: { (connection, result, error) in
+                if error == nil {
+                    print("User Info : \(result)")
+                } else {
+                    print("Error Getting Info \(error)");
+                }
+            })
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+       ChnageButtonText(btntitle:"Logging")
+    }
+    
+}
