628cd7b415d3bdabe7a23a1c257870804c929c1d
mgdperera <mgdperera@gmail.com>
2018-12-31 21:19:10 +0530

    add user default class

---
+++ SampleIOSApp/Utility/UserDefault.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefault.swift
+//  SampleIOSApp
+//
+//  Created by gayan perera on 12/31/18.
+//  Copyright © 2018 gayan perera. All rights reserved.
+//
+
+import Foundation
+
+class UserDefault {
+    
+    
+    
+    
+    
+}
